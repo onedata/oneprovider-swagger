6599d5181453c74b22c059d83b00daf161238cc8
Bartosz Kryza <bkryza@gmail.com>
2016-05-30 15:43:59 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Generate aggregate JSON file from YAML
+docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.4.0
+
+# Validate the JSON
+docker run --rm -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.2.0 validate /swagger/swagger.json
+
+# Generate the Cowboy server stub
+docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.2.0 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
+
+# Generate C# stub to get all moustache tempalte keywords
+#swagger-codegen-dbg generate -i ./swagger.json -l csharp -o ./generated/csharp -o tmp > model.json
+
+# Generate the static documentation
+docker run --rm -v `pwd`:/swagger -t docker.onedata.org/swagger-bootprint:1.1.0 swagger ./swagger.json generated/static
+
