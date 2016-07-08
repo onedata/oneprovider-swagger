7b5d76e28bc84df44fa3201de588660393504f6a
Michal Orzechoski <michal.orzechowski@cyfronet.pl>
2016-07-08 11:13:30 +0000

    updated summary.md for new swagger workflow

--- release.sh
+++ release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-source update_summary.sh 
 
 if [[ $# -lt 1 ]]; then
-  DOC_BRANCH="swagger-test"
+  DOC_BRANCH="develop"
 else
   DOC_BRANCH=$1
 fi
@@ -15,18 +14,20 @@ $git_c clone  --single-branch -b $DOC_BRANCH ssh://git@git.plgrid.pl:7999/vfs/${
 SUMMARY=${TMP_DIR}/SUMMARY.md
 
 SRC_GITBOOK=generated/gitbook
-PATH_IN_GITBOOK=${TMP_DIR}/doc/advanced/rest/onepanel
+PATH_IN_GITBOOK=${TMP_DIR}/doc/advanced/rest/oneprovider
 $git_c rm -r $PATH_IN_GITBOOK/*
-
 mkdir -p $PATH_IN_GITBOOK
-update_summary $SUMMARY ONEPANEL_PATHS $SRC_GITBOOK/PATHS_TOC.md
-update_summary $SUMMARY ONEPANEL_DEFINITIONS $SRC_GITBOOK/DEFINITIONS_TOC.md
-
 cp -r $SRC_GITBOOK/* $PATH_IN_GITBOOK/
-ls $PATH_IN_GITBOOK/
+
+SRC_SWAGGER=swagger.json
+PATH_IN_GITBOOK=${TMP_DIR}/doc/swagger/oneprovider
+$git_c rm -r $PATH_IN_GITBOOK/*
+mkdir -p $PATH_IN_GITBOOK
+cp -r $SRC_SWAGGER/* $PATH_IN_GITBOOK/
+
 $git_c add -A
 $git_c diff 
-$git_c commit -m "updated onepanel swagger api"
+$git_c commit -m "updated oneprovider swagger api"
 $git_c push
 
 rm -rf $TMP_DIR 
