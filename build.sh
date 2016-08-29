74160ab4136f5c649b0fdf41a00c9ed2f5b07201
Bartosz Kryza <bkryza@gmail.com>
2016-08-29 13:37:19 +0200

    Upgraded build script

--- build.sh
+++ build.sh
@@ -4,11 +4,9 @@
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-aggregator:1.5.0
 
 # Validate the JSON
-docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json
-# Validate the JSON
 output=$(docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger docker.onedata.org/swagger-cli:1.5.0 validate /swagger/swagger.json 2>&1)
 
-if [[ $output =~ .*Swagger\ schema\ validation\ failed.* ]]; then
+if [[ $output =~ .*SyntaxError.* ]] ; then
   echo "Generated swagger.json is not valid - check YAML sources:\n"
   echo $output
   exit 1
@@ -16,6 +14,8 @@ fi
 
 # Generate the Cowboy server stub
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-codegen:1.5.3 generate -i ./swagger.json -l cowboy -o ./generated/cowboy
+
+# Fix generated Erlang files
 ./fix_generated.py
 
 # Generate C# stub to get all moustache tempalte keywords
@@ -24,7 +24,10 @@ docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swa
 # Generate the static documentation
 docker run --rm -e "CHOWNUID=${UID}" -v `pwd`:/swagger -t docker.onedata.org/swagger-bootprint:1.5.0 swagger ./swagger.json generated/static
 
-sed -n '/<body>/,/<\/body>/p' generated/static/index.html | sed -e '1s/.*<body>//' -e '$s/<\/body>.*//' -e 's/\/definitions\//definitions--/g' -e 's/<div class=\"container\"/<div class=\"container swagger\"/' > generated/static/oneprovider-static.html
+#cp generated/static/index.html generated/static/oneprovider-static.html
+sed -n '/<body>/,/<\/body>/p' generated/static/index.html \
+  | sed -e '1s/.*<body>//' -e '$s/<\/body>.*//' -e 's/\/definitions\//definitions--/g' -e 's/<div class=\"container\"/<div class=\"container swagger\"/' \
+  > generated/static/oneprovider-static.html
 
 # Generate Markdown for direct Gitbook integration
 # The output from generated/gitbook should be copied to onedata-documentation/doc/advanced/rest/oneprovider folder
