e8a1ab01757b2ee60edf8c73ebe378196b953544
Bartosz Kryza <bkryza@gmail.com>
2016-10-20 13:42:33 +0200

    Fixed missing body parameters and improved comments

--- README.md
+++ README.md
@@ -22,3 +22,41 @@ node resolve.js > swagger.json
 
 After any of these steps you should have a complete `swagger.json` file, with specification of Oneprovider REST API. The file can be used by Swagger [code generator](https://github.com/swagger-api/swagger-codegen) to produce example code for clients in various languages or viewed online using for instance [Swagger Online Editor](http://editor.swagger.io/).
 
+
+## Using generated Bash client
+
+Below are command line examples for generated bash client.
+
+```shell
+export ONEPROVIDER_HOST=https://PROVIDER_IP:8443
+export ONEPROVIDER_BASIC_AUTH="username:password"
+
+./oneprovider-rest-cli -h
+
+./oneprovider-rest-cli getAllSpaces -h
+
+./oneprovider-rest-cli getAllSpaces
+
+
+./oneprovider-rest-cli getFileAttributes path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" attribute=mode
+
+
+./oneprovider-rest-cli getFileAttributes path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" attribute=license extended=true
+
+
+./oneprovider-rest-cli getFileMetadata -ac json path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg"
+
+
+./oneprovider-rest-cli listFiles path="/OxfordFlowerDataset"
+
+./oneprovider-rest-cli listFiles path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg"
+
+./oneprovider-rest-cli -ac json getFileMetadataById id=0000000000917733836803640004677569646D0000005254576F796132686E575530775331424F516B5534646B5A4963565678644545795746684F616E646B5157466A615531484D565A33516E42785A794D6A6E7A69434565634B2D494A4679387770456E6E3850776D0000002B6136552D334675766A6A6C465A7251576D7639584256304E454448594A615A4C4D6132725A566F704F746B
+
+echo '{"name":"Flower2","genus":"Astragalus"}' | ./oneprovider-rest-cli -ct json setFileMetadata path="/OxfordFlowerDataset/FlowerSet1/image_0801.jpg"
+
+./oneprovider-rest-cli getAllTransfers
+
+./oneprovider-rest-cli getFileReplicas path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg"
+
+```
---
+++ tests/bash-swagger-test.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bats
+
+
+export ONEPROVIDER_HOST="https://localhost:9443"
+export ONEPROVIDER_API_KEY="SECRET"
+
+export ONEPROVIDER_CLI=generated/bash/oneprovider-rest-cli
+
+#
+# Tests generation of curl requests
+#
+@test "getAllSpaces Api key passed as header" {
+    run bash $ONEPROVIDER_CLI getAllSpaces --dry-run X-Auth-Key:SECRET
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/spaces" ]]
+    [[ "$output" =~ "-X GET" ]]
+    [[ "$output" =~ "X-Auth-Key: SECRET" ]]
+}
+
+@test "getAllSpaces Api key from environment variable" {
+    run bash $ONEPROVIDER_CLI getAllSpaces --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/spaces" ]]
+    [[ "$output" =~ "-X GET" ]]
+    [[ "$output" =~ "X-Auth-Key: SECRET" ]]
+}
+
+@test "getAllSpaces username and password" {
+    run bash $ONEPROVIDER_CLI -u username:password getAllSpaces --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/spaces" ]]
+    [[ "$output" =~ "-X GET" ]]
+    [[ "$output" =~ "-u username:password" ]]
+}
+
+@test "getFileAttributes not extended" {
+    run bash $ONEPROVIDER_CLI getFileAttributes path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" attribute=mode --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/attributes//OxfordFlowerDataset/FlowerSet1/image_0800.jpg?attribute=mode" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
+@test "getFileAttributes extended" {
+    run bash $ONEPROVIDER_CLI getFileAttributes path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" attribute=license extended=true --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/attributes//OxfordFlowerDataset/FlowerSet1/image_0800.jpg?" ]]
+    [[ "$output" =~ "attribute=license" ]]
+    [[ "$output" =~ "extended=true" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
+@test "getFileAttributes extended with spaces" {
+    run bash $ONEPROVIDER_CLI getFileAttributes path="/Oxford Flower Dataset/Flower Set1/image_0800.jpg" attribute=license extended=true --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/attributes//Oxford%20Flower%20Dataset/Flower%20Set1/image_0800.jpg?" ]]
+    [[ "$output" =~ "attribute=license" ]]
+    [[ "$output" =~ "extended=true" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
+@test "getFileMetadata json" {
+    run bash $ONEPROVIDER_CLI getFileAttributes -ac json path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/attributes//OxfordFlowerDataset/FlowerSet1/image_0800.jpg" ]]
+    [[ "$output" =~ "-H 'Accept: application/json'" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
+@test "getFileMetadata rdf" {
+    run bash $ONEPROVIDER_CLI getFileAttributes -ac rdf path="/OxfordFlowerDataset/FlowerSet1/image_0800.jpg" --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/attributes//OxfordFlowerDataset/FlowerSet1/image_0800.jpg" ]]
+    [[ "$output" =~ "-H 'Accept: application/rdf+xml'" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
+@test "setFileMetadata from file" {
+    run bash ${ONEPROVIDER_CLI} -ct json -d @metadata.json setFileMetadata path="/OxfordFlowerDataset/FlowerSet1/image_0801.jpg" --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/metadata//OxfordFlowerDataset/FlowerSet1/image_0801.jpg" ]]
+    [[ "$output" =~ "-H 'Content-type: application/json'" ]]
+    [[ "$output" =~ "-d @metadata.json" ]]
+    [[ "$output" =~ "-X PUT" ]]
+}
+
+@test "querySpaceIndexes attributes" {
+    run bash ${ONEPROVIDER_CLI} querySpaceIndexes iid="1234-1234-1234-1234" endkey_docid=123 inclusive_end=45 --dry-run
+    [[ "$output" =~ "${ONEPROVIDER_HOST}/api/v3/oneprovider/query-index/1234-1234-1234-1234" ]]
+    [[ "$output" =~ "endkey_docid=123" ]]
+    [[ "$output" =~ "-X GET" ]]
+}
+
